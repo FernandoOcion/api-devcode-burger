20291884961851c13307ed981fec1cf290bb57e6
Fernando Ocion <154288469+FernandoOcion@users.noreply.github.com>
2024-09-23 23:34:34 -0300

    Update routes.js

--- src/routes.js
+++ src/routes.js
@@ -1,19 +1,30 @@
-import Router from "express";
-import multer from "multer";
-import authMiddleware from "./app/Middlewares/auth";
-import multerConfig from "./config/multer";
+const Router = require("express");
+const multer = require("multer");
+const authMiddleware = require("./app/Middlewares/auth.js");
+const multerConfig = require("./config/multer.cjs");
 
-import CategoryController from "./app/controllers/CategoryController";
-import OrderController from "./app/controllers/OrderController";
-import ProductController from "./app/controllers/ProductController";
-import SessionController from "./app/controllers/SessionController";
-import UserController from "./app/controllers/UserController";
+const CategoryController = require("./app/controllers/CategoryController.js");
+const OrderController = require("./app/controllers/OrderController.js");
+const ProductController = require("./app/controllers/ProductController.js");
+const SessionController = require("./app/controllers/SessionController.js");
+const UserController = require("./app/controllers/UserController.js");
+
+// import Router from "express";
+// import multer from "multer";
+// import authMiddleware from "./app/Middlewares/auth.js";
+// import multerConfig from "./config/multer.cjs";
+
+// import CategoryController from "./app/controllers/CategoryController.js";
+// import OrderController from "./app/controllers/OrderController.js";
+// import ProductController from "./app/controllers/ProductController.js";
+// import SessionController from "./app/controllers/SessionController.js";
+// import UserController from "./app/controllers/UserController.js";
 
 const routes = new Router();
 
-routes.get("/", (req, res) => {
-	return res.json({ message: "Hello to my first API" });
-});
+// routes.get("/", (req, res) => {
+// 	return res.json({ message: "Hello to my first API" });
+// });
 
 const upload = multer(multerConfig);
 
@@ -33,4 +44,4 @@ routes.post("/orders", OrderController.store);
 routes.get("/orders", OrderController.index);
 routes.put("/orders/:id", OrderController.update);
 
-export default routes;
+module.exports = routes;
